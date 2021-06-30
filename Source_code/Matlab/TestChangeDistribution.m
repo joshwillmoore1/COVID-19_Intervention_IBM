1a90e180b98f9807dd3692cc8db8c7eabb4000bb
joshwillmoore1 <45179849+joshwillmoore1@users.noreply.github.com>
2021-06-30 10:48:30 +0100

    Update TestChangeDistribution.m

--- Source_code/Matlab/TestChangeDistribution.m
+++ Source_code/Matlab/TestChangeDistribution.m
@@ -25,14 +25,6 @@ SocialDistancingModifier = 1;
 Para = [Ntot,SubgroupSize,Irb,R,Cf,Cc,PerFalseNeg,CompIso,MixInd,TestInd...
     ,PrevBool,RecoveryBool,PerSymptomatic,RemoveSympt,PerFalsePos,InitWithTesting,SocialDistancingModifier ];
 
-Para08 = [Ntot,SubgroupSize,Irb,R,Cf,Cc,PerFalseNeg,CompIso,MixInd,TestInd...
-    ,PrevBool,RecoveryBool,PerSymptomatic,RemoveSympt,PerFalsePos,InitWithTesting,SocialDistancingModifier, 0.4 ];
-
-Para1 = [Ntot,SubgroupSize,Irb,R,Cf,Cc,PerFalseNeg,CompIso,MixInd,TestInd...
-    ,PrevBool,RecoveryBool,PerSymptomatic,RemoveSympt,PerFalsePos,InitWithTesting,SocialDistancingModifier,0.6 ];
-
-Para12 = [Ntot,SubgroupSize,Irb,R,Cf,Cc,PerFalseNeg,CompIso,MixInd,TestInd...
-    ,PrevBool,RecoveryBool,PerSymptomatic,RemoveSympt,PerFalsePos,InitWithTesting,SocialDistancingModifier,0.8 ];
 
 tic
 [MeanNumOfHealthy,MeanTotalInfections,MeanNumberOfSympt...
@@ -40,23 +32,6 @@ tic
     MeanNumberOfCasesDetected,MeanProportionOfInfectedIsolations] = General_Delay_LFT(Para);
 toc
 
-tic
-[MeanNumOfHealthy_ln,MeanTotalInfections_ln08,MeanNumberOfSympt_ln...
-    ,MeanNumberOfAsympt_ln,initTestingDay_ln,MeanNumOfRecoveries_ln,...
-    MeanNumberOfCasesDetected_ln08,MeanProportionOfInfectedIsolations_ln] = General_Delay_LFT_LN_NORM(Para08);
-toc
-
-tic
-[MeanNumOfHealthy_ln,MeanTotalInfections_ln1,MeanNumberOfSympt_ln...
-    ,MeanNumberOfAsympt_ln,initTestingDay_ln,MeanNumOfRecoveries_ln,...
-    MeanNumberOfCasesDetected_ln1,MeanProportionOfInfectedIsolations_ln] = General_Delay_LFT_LN_NORM(Para1);
-toc
-
-tic
-[MeanNumOfHealthy_ln,MeanTotalInfections_ln12,MeanNumberOfSympt_ln...
-    ,MeanNumberOfAsympt_ln,initTestingDay_ln,MeanNumOfRecoveries_ln,...
-    MeanNumberOfCasesDetected_ln12,MeanProportionOfInfectedIsolations_ln] = General_Delay_LFT_LN_NORM(Para12);
-toc
 
 
 %% plot the results
@@ -64,24 +39,14 @@ close all
 figure()
 subplot(1,2,1)
 plot(1:28,MeanTotalInfections)
-hold on
-plot(1:28,MeanTotalInfections_ln08)
-plot(1:28,MeanTotalInfections_ln1)
-plot(1:28,MeanTotalInfections_ln12)
 ylabel({"Mean number"," of total infections"})
 xlabel("Number of days")
-legend("Poisson","Log-normal $\sigma = 0.4$","Log-normal $\sigma = 0.6$","Log-normal $\sigma = 0.8$",'location','best')
 ylim([0,Ntot])
 
 
 subplot(1,2,2)
 plot(1:28,MeanNumberOfCasesDetected)
-hold on
-plot(1:28,MeanNumberOfCasesDetected_ln08)
-plot(1:28,MeanNumberOfCasesDetected_ln1)
-plot(1:28,MeanNumberOfCasesDetected_ln12)
 ylabel({"Mean number" ,"of isolating individuals"})
 xlabel("Number of days")
-legend("Poisson","Log-normal $\sigma = 0.4$","Log-normal $\sigma = 0.6$","Log-normal $\sigma = 0.8$",'location','best')
 ylim([0,Ntot])
 
